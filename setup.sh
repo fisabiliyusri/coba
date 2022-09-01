b6802609d046a8c8477ce5da386a9464694447ca
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-01 09:49:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-#setup
+# Setup
+# auto installer script created by SL
+# skrip penginstal otomatis dibuat oleh SL
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+# ==========================================
+# Color
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
