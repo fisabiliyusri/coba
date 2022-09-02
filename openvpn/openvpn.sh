eac702562d230ea4f979796dfc45314e209249b5
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-02 13:38:02 +0700

    Update openvpn.sh

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+# ==================================================
+# Link Hosting Kalian
+akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/ssh"
 
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+source /etc/os-release
+ver=$VERSION_ID
+
+#detail nama perusahaan
+country=ID
+state=Indonesia
+locality=Indonesia
+organization=infinity
+organizationalunit=infinity
+commonname=localhost
+email=hayuk69@gmail.com
+
+#
+wget https://${akbarvpn}/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
