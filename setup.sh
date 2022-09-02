7b876ee460f015d3c888e0c0cb93177ccb8a1dd4
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-02 09:45:22 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -155,4 +155,55 @@ systemctl restart nginx
 
 
 # Status
-
+#SELESAI
+echo " "
+echo "Installation has been completed!!"echo " "
+echo "============================================================================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "----------------------------------------------------------------------------" | tee -a log-install.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"  | tee -a log-install.txt
+echo -e "    SCRIPT MANTAP-XRAY Multi Port"  | tee -a log-install.txt
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - SSH OpenSSH                : 22,99,169"  | tee -a log-install.txt
+echo "   - SSH Dropbear               : 109,143,300,1153,"  | tee -a log-install.txt
+echo "   - SSH SlowDNS                : 2269,3469"  | tee -a log-install.txt
+echo "   - SSH Websocket HTTP         : 80"  | tee -a log-install.txt
+echo "   - SSH Websocket SSL/TLS      : 443"  | tee -a log-install.txt
+echo "   - Stunnel5                   : 445"  | tee -a log-install.txt
+echo "   - SSLH                       : 8000"  | tee -a log-install.txt
+echo "   - Nginx                      : 89,80,443"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN WS TLS        : 443"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS WS TLS   : 443"  | tee -a log-install.txt
+echo "   - XRAYS VLESS WS TLS         : 443"  | tee -a log-install.txt
+echo "   - XRAYS VMESS WS TLS         : 443"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN GRPC          : 443"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS GRPC     : 443"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC           : 443"  | tee -a log-install.txt
+echo "   - XRAYS VLESS GRPC           : 443"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN WS HTTP       : 80"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS WS HTTP  : 80"  | tee -a log-install.txt
+echo "   - XRAYS VLESS WS HTTP        : 80"  | tee -a log-install.txt
+echo "   - XRAYS VMESS WS HTTP        : 80"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN GRPC          : 80"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS GRPC     : 80"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC           : 80"  | tee -a log-install.txt
+echo "   - XRAYS VLESS GRPC           : 80"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo " Reboot 15 Sec"
+sleep 15
