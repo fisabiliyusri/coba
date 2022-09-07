d79b1119e675695b4e07d01d38148303c401b769
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-07 12:25:13 +0700

    Update okep.sh

--- crot/okep.sh
+++ crot/okep.sh
@@ -1,49 +1,4 @@
 #!/bin/bash
-#wget https://github.com/${GitUser}/
-GitUser="RazVpn"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/ipv3/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/ipv3/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
-clear
-
-# EMAIL & DOMAIN
-emailcf=$(cat /usr/local/etc/xray/email)
-domain=$(cat /root/domain)
-
-# ACME DOMAIN
-echo ""
-echo -e "     \e[1;32m1)\e[0m Letsencrypt.org"
-echo -e "   ------------------------------------"
-read -p "   Please select numbers 1-3(Any Button Default Letsencrypt.org) : " acmee
-acme3=letsencrypt
-if [[ $acmee == "2" ]]; then
-echo -e "Letsencrypt.org acme is used"
-acmeh=$acme3
-else
-echo -e "Default acme(Letsencrypt.org) is used"
-acmeh=$acme3
-clear
-fi
 clear
 
 apt install iptables iptables-persistent -y
@@ -59,9 +14,6 @@ chronyc sourcestats -v
 chronyc tracking -v
 date
 
-# Acc Trojan XRAY TCP TLS
-mkdir -p /usr/local/etc/xray/
-touch /usr/local/etc/xray/akunxtr.conf
 
 # CHECK VERSION XRAY CORE
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
@@ -86,22 +38,15 @@ mkdir -p /var/log/xray/
 # Stop port 80
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 
-# generate certificates
-mkdir /root/.acme.sh
-curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
-chmod +x /root/.acme.sh/acme.sh
-/root/.acme.sh/acme.sh --server $acmeh \
-        --register-account  --accountemail $emailcf
-/root/.acme.sh/acme.sh --server $acmeh --issue -d $domain --standalone -k ec-256			   
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
-service squid start
-uuid1=$(cat /proc/sys/kernel/random/uuid)
-uuid2=$(cat /proc/sys/kernel/random/uuid)
-uuid3=$(cat /proc/sys/kernel/random/uuid)
-uuid4=$(cat /proc/sys/kernel/random/uuid)
-uuid5=$(cat /proc/sys/kernel/random/uuid)
-uuid6=$(cat /proc/sys/kernel/random/uuid)
-cat> /usr/local/etc/xray/config.json << END
+#
+uuid9=$(cat /proc/sys/kernel/random/uuid)
+uuid=b8458948-a630-4e6d-809a-230b2223ff3d
+
+# // Certificate File
+path_crt="/etc/xray/xray.crt"
+path_key="/etc/xray/xray.key"
+# buat config server
+cat> /etc/xray/xrayconfig.json << END
 {
   "log": {
     "access": "/var/log/xray/access.log",
@@ -110,12 +55,12 @@ cat> /usr/local/etc/xray/config.json << END
        },
     "inbounds": [
         {
-            "port": 443,
+            "port": 888,
             "protocol": "vless",
             "settings": {
                 "clients": [
                     {
-                        "id": "${uuid1}",
+                        "id": "${uuid}",
                         "flow": "xtls-rprx-direct",
                         "level": 0
 #xray-vless-xtls
@@ -148,8 +93,8 @@ cat> /usr/local/etc/xray/config.json << END
                     ],
                     "certificates": [
                         {
-                            "certificateFile": "/usr/local/etc/xray/xray.crt",
-                            "keyFile": "/usr/local/etc/xray/xray.key"
+                            "certificateFile": "${path_crt}",
+                            "keyFile": "${path_key}"
                         }
                     ]
                 }
@@ -162,8 +107,8 @@ cat> /usr/local/etc/xray/config.json << END
             "settings": {
                 "clients": [
                     {
-                        "id": "${uuid2}",
-                        "password": "xxxxxx"
+                        "id": "${uuid}",
+                        "password": "${uuid}"
 #trojan
                     }
                 ],
@@ -188,7 +133,7 @@ cat> /usr/local/etc/xray/config.json << END
             "settings": {
                 "clients": [
                     {
-                        "id": "${uuid3}",
+                        "id": "${uuid}",
                         "alterId": 0,
                         "level": 0
 #xray-vmess-tls
@@ -298,152 +243,11 @@ cat> /usr/local/etc/xray/config.json << END
   }
 }
 END
-cat> /usr/local/etc/xray/none.json << END
-{
-  "log": {
-    "access": "/var/log/xray/access.log",
-    "error": "/var/log/xray/error.log",
-    "loglevel": "info"
-  },
-  "inbounds": [
-    {
-            "port": 80,
-            "protocol": "vmess",
-            "settings": {
-            "clients": [
-                {
-                  "id": "${uuid4}",
-                  "alterId": 0,
-                  "level": 0
-#xray-vmess-nontls
-                }
-            ]
-        },
-        "streamSettings": {
-            "network": "ws",
-            "security": "none",
-            "wsSettings": {
-                "path": "/xray-vmessws-none-tls",
-                "headers": {
-                   "Host": ""
-                 }
-             },
-             "quicSettings": {}
-          },
-          "sniffing": {
-             "enabled": true,
-             "destOverride": [
-                "http",
-                "tls"
-                ]
-            }
-        },
-        {
-            "port": 8080,
-            "protocol": "vless",
-            "settings": {
-            "clients": [
-                {
-                  "id": "${uuid6}"
-#xray-vless-nontls
-                }
-            ],
-            "decryption": "none"
-         },
-         "streamSettings": {
-            "network": "ws",
-            "security": "none",
-            "wsSettings": {
-            "path": "/xray-vlessws-none-tls",
-            "headers": {
-                "Host": ""
-               }
-            },
-            "quicSettings": {}
-          },
-          "sniffing": {
-              "enabled": true,
-              "destOverride": [
-                 "http",
-                 "tls"
-             ]
-          }
-       }
-    ],
-    "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
-    }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "inboundTag": [
-          "api"
-        ],
-        "outboundTag": "api",
-        "type": "field"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
-  },
-  "stats": {},
-  "api": {
-    "services": [
-      "StatsService"
-    ],
-    "tag": "api"
-  },
-  "policy": {
-    "levels": {
-      "0": {
-        "statsUserDownlink": true,
-        "statsUserUplink": true
-      }
-    },
-    "system": {
-      "statsInboundUplink": true,
-      "statsInboundDownlink": true
-    }
-  }
-}
-END
+
 # starting xray vmess ws tls core on sytem startup
-cat> /etc/systemd/system/xray.service << END
+cat> /etc/systemd/system/xraysuper.service << END
 [Unit]
-Description=Xray Service
+Description=Xray Super Service
 Documentation=https://github.com/xtls
 After=network.target nss-lookup.target
 
@@ -452,7 +256,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/xray run -config /usr/local/etc/xray/config.json
+ExecStart=/usr/local/bin/xray run -config /etc/xray/xrayconfig.json
 Restart=on-failure
 RestartPreventExitStatus=23
 LimitNPROC=10000
@@ -462,73 +266,11 @@ LimitNOFILE=1000000
 WantedBy=multi-user.target
 
 END
-
-# starting xray vmess ws tls core on sytem startup
-cat> /etc/systemd/system/xray@.service << END
-[Unit]
-Description=Xray Service
-Documentation=https://github.com/xtls
-After=network.target nss-lookup.target
-
-[Service]
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/local/bin/xray run -config /usr/local/etc/xray/%i.json
-Restart=on-failure
-RestartPreventExitStatus=23
-LimitNPROC=10000
-LimitNOFILE=1000000
-
-[Install]
-WantedBy=multi-user.target
-
-END
-
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1310 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1311 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1312 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8080 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1310 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1311 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1312 -j ACCEPT
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
 
 # enable xray tls
 systemctl daemon-reload
-systemctl enable xray.service
-systemctl start xray.service
-systemctl restart xray
-
-# enable xray none tls
-systemctl daemon-reload
-systemctl enable xray@none
-systemctl start xray@none
-systemctl restart xray@none
-
-# download script
-cd /usr/bin
-wget -O port-xray "https://raw.githubusercontent.com/${GitUser}/V3/main/change-port/port-xray.sh"
-wget -O port-trojan "https://raw.githubusercontent.com/${GitUser}/V3/main/change-port/port-trojan.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/${GitUser}/V3/main/cert.sh"
-wget -O trojaan "https://raw.githubusercontent.com/${GitUser}/V3/main/menu/trojaan.sh"
-wget -O xraay "https://raw.githubusercontent.com/${GitUser}/V3/main/menu/xraay.sh"
-chmod +x port-xray
-chmod +x port-trojan
-chmod +x certv2ray
-chmod +x trojaan
-chmod +x xraay
-
+systemctl enable xraysuper.service
+systemctl start xraysuper.service
+systemctl restart xraysuper
+#
 cd
-rm -f ins-xray.sh
-mv /root/domain /usr/local/etc/xray/domain
-
